d15920d7372d7efccdd588bc17840403168fd743
yfh01 <987751945@qq.com>
2026-01-03 15:09:53 +0800

    refactor: update FontGlyph structure for improved memory layout
    Modified the FontGlyph structure to replace the bitfields with a more explicit representation using bitfield units and added a new pack_id field. Updated memory layout tests to reflect these changes, enhancing clarity and ensuring proper alignment in the glyph representation.

--- imgui/src/fonts/glyph.rs
+++ imgui/src/fonts/glyph.rs
@@ -5,7 +5,8 @@ use crate::sys;
 #[derive(Copy, Clone, Debug, PartialEq)]
 #[repr(C)]
 pub struct FontGlyph {
-    bitfields: u32,
+    pub _bitfield_align_1: [u32; 0],
+    pub _bitfield_1: sys::__BindgenBitfieldUnit<[u8; 4usize]>,
     pub advance_x: f32,
     pub x0: f32,
     pub y0: f32,
@@ -15,6 +16,7 @@ pub struct FontGlyph {
     pub v0: f32,
     pub u1: f32,
     pub v1: f32,
+    pub pack_id: i32,
 }
 
 impl FontGlyph {
@@ -54,7 +56,8 @@ fn test_font_glyph_memory_layout() {
             );
         };
     }
-    assert_field_offset!(bitfields, _bitfield_1);
+    assert_field_offset!(_bitfield_align_1, _bitfield_align_1);
+    assert_field_offset!(_bitfield_1, _bitfield_1);
     assert_field_offset!(advance_x, AdvanceX);
     assert_field_offset!(x0, X0);
     assert_field_offset!(y0, Y0);
@@ -64,4 +67,5 @@ fn test_font_glyph_memory_layout() {
     assert_field_offset!(v0, V0);
     assert_field_offset!(u1, U1);
     assert_field_offset!(v1, V1);
+    assert_field_offset!(pack_id, PackId);
 }
